cb524c48a11c68190557aae2d678be8a6134fcb0
JamKelley22 <jkelley@iastate.edu>
2018-11-11 11:42:26 -0600

    relitive path build

--- v2.0.0/build/service-worker.js
+++ v2.0.0/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./v2.0.0/precache-manifest.9adbeba11bb6ed1f5944ff073fae075c.js"
+  "/precache-manifest.9e067b808776aa232a9adf9f303cdedc.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./v2.0.0/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
