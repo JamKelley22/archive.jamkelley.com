22b9b0c59bfd65bcbe092bfb1d090bc285132e5e
JamKelley22 <jkelley@iastate.edu>
2018-11-11 13:51:56 -0600

    removed build/ from .gitignore

---
+++ v2.1.0/build/precache-manifest.97105e6066b44de877198f38cd1d366b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "deb089b066f8f32967e08079131c513e",
+    "url": "/static/media/ebet.deb089b0.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c7b47da0a011d36ee20a",
+    "url": "/static/js/main.c7b47da0.chunk.js"
+  },
+  {
+    "revision": "1a61f277a2473466ae60",
+    "url": "/static/js/1.1a61f277.chunk.js"
+  },
+  {
+    "revision": "c7b47da0a011d36ee20a",
+    "url": "/static/css/main.6aeebf95.chunk.css"
+  },
+  {
+    "revision": "5f39d9e14c5f355e7d12060bd5c85a77",
+    "url": "/index.html"
+  }
+];
