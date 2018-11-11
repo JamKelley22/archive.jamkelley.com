cf7f428b01b68b7781b08e96c3a4fd24a5e39db1
JamKelley22 <jkelley@iastate.edu>
2018-11-11 13:17:08 -0600

    homepage ./

--- v2.0.0/build/service-worker.js
+++ v2.0.0/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9e067b808776aa232a9adf9f303cdedc.js"
+  "./precache-manifest.ddb26e934856afa6a4464705a54cbc92.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
