4c64507ae9ccaf37c70ccf2a1490f9bda248ed04
JamKelley22 <jkelley@iastate.edu>
2018-11-11 11:38:06 -0600

    relitive path build

---
+++ v2.0.0/build/precache-manifest.9adbeba11bb6ed1f5944ff073fae075c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "deb089b066f8f32967e08079131c513e",
+    "url": "./v2.0.0/static/media/ebet.deb089b0.png"
+  },
+  {
+    "revision": "8c4409d7197974a3606a",
+    "url": "./v2.0.0/static/js/runtime~main.8c4409d7.js"
+  },
+  {
+    "revision": "4b0f32f2687daa0aaee5",
+    "url": "./v2.0.0/static/js/main.4b0f32f2.chunk.js"
+  },
+  {
+    "revision": "afeabcb9dca84ff14c30",
+    "url": "./v2.0.0/static/js/1.afeabcb9.chunk.js"
+  },
+  {
+    "revision": "4b0f32f2687daa0aaee5",
+    "url": "./v2.0.0/static/css/main.27c75969.chunk.css"
+  },
+  {
+    "revision": "0c2ef7c3160bb6a2f5430fb18346cd41",
+    "url": "./v2.0.0/index.html"
+  }
+];
