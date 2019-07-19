f8d6bca61cfdc233f1abcf5917aba0711208f99f
Jameel Kelley <jameel.kelley@buildertrend.com>
2019-07-19 09:14:49 -0500

    Added v3.0.0

---
+++ v3.0.0/src/components/index.js
@@ -0,0 +1,9 @@
+import ChatBot from './ChatBot/chatbot.js'
+import GitHub from './GitHub/github.js'
+import { Video } from './YouTube'
+import Settings from './Settings/settings.js'
+import ModelViewer from './ModelViewer/modelViewer.js'
+import Sidebar from './Sidebar/sidebar.js'
+import QOTD from './QOTD/qotd.js'
+
+export { ChatBot, GitHub, Video, Settings, ModelViewer, Sidebar, QOTD }
